4a4e6ef3f40e375684a29b1d5911b4da404fb18e
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2024-06-20 17:36:22 +0800

    added login endpoint

--- api.py
+++ api.py
@@ -1,3 +1,4 @@
+import os, sys, json, datetime, copy
 from flask import Blueprint, request, jsonify, redirect, url_for, render_template, session
 from main import DI, Universal
 
@@ -9,6 +10,24 @@ def checkCredentials(requestBody):
     if "password" not in requestBody:
         return "ERROR: Password not provided."
 
+    for account in DI.data["accounts"]:
+        if DI.data["accounts"][account]["email"] == requestBody["email"] and DI.data["accounts"][account]["password"] == requestBody["password"]:
+            return True
+        
+    return "ERROR: Invalid credentials."
+
 @apiBP.route("/api/health", methods=["GET"])
 def health():
     return "SUCCESS: Healthy!", 200
+
+@apiBP.route("/api/login", methods=["POST"])
+def login():
+    if not request.is_json:
+        return "ERROR: Request body is not JSON.", 400
+    
+    requestBody = copy.deepcopy(request.json)
+    response = checkCredentials(requestBody)
+    if response != True:
+        return response, 401
+
+    return "SUCCESS: Logged in.", 200
